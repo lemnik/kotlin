0113ac0f755571091916ced96dfa2e751471654a
jason <lemnik@lemnik.com>
2019-09-24 07:28:57 +0100

    Added a secondary check for `module` instead of just `exports` which eliminates all errors from "closure-compiler" when run over `kotlin.js`

--- libraries/stdlib/js-v1/src/wrapper.js
+++ libraries/stdlib/js-v1/src/wrapper.js
@@ -2,7 +2,7 @@
     if (typeof define === 'function' && define.amd) {
         define('kotlin', ['exports'], factory);
     }
-    else if (typeof exports === 'object') {
+    else if (typeof module === 'object' && typeof module.exports === 'object') {
         factory(module.exports);
     }
     else {
